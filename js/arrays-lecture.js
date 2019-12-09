b1e8df1788fb260fd72fa6b0f792d6f234d72f38
Miguel Vera <miguel.vera7908@gmail.com>
2019-12-09 15:49:11 -0600

    Added arrays-lecture.html and arrays-lecture.js

---
+++ js/arrays-lecture.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// var pets = [];
+// pets[1] = "Toby";
+// pets[2] = "Princess";
+// pets[3] = "Samson";
+// pets[4] = "Jill";
+// pets[5] = "Bubbles";
+// pets[6] = "Malu";
+
+var pets = ["Toby","Princess","Samson","Jill","Bubbles","Malu","Lily"];
+
+for (var i = 0; i <pets.length; i++){
+    console.log(pets[i]);
+}
