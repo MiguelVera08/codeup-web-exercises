793c780f63fd1cb0ced4a37c8dea99a3d51a198e
Miguel Vera <miguel.vera7908@gmail.com>
2020-01-22 12:01:46 -0600

    Added Module lecture example

---
+++ js/math.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// math.js
+const add = (x, y) => x + y;
+const subtract = (x, y) => y - x;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+module.exports = {add, subtract, multiply, divide}; // es6 shorthand
